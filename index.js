17cd5a17d41eeea0e92d6da27bfc9a0556adad41
Abiodun001-world <adekunleabiodun354@gmail.com>
2022-10-29 17:15:56 +0100

    fixing bugs

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 //Declaring a variable saveEl,countEl and count
-let saveEl = document.getElementById("save-el"),
-let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el");
+let countEl = document.getElementById("count-el");
 let count = 0
+
 // Adding a fubnction to incrememt
 function increment() {
     count += 1
     countEl.textContent = count
 }
+
 //Adding a function to Decrement 
 function decrement() {
    count -=1;
    countEl.textContext = count
 }
+
 //Adding a function to save
 function save() {
     let countStr = count + " - "
